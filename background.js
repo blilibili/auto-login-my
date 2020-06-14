4978b147f91e02712f9cc2007511019368fa8209
448150895@qq.com <448150895@qq.com>
2020-06-14 23:40:18 +0800

    'update'

---
+++ background.js
@@ -0,0 +1,10 @@
+/**
+ * Created by liyigang on 1/5/2020.
+ */
+chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
+    console.log(request, sender, sendResponse)
+    if(request.type === 'REQUEST_USERS_ALL'){
+        console.log('我是content-script,收到来自popup的消息：' + request.title);
+        sendResponse('我是content-script, 已收到你的预览全部图片的消息');
+    }
+});
